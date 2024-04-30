4bf0627324c91d4c6271b89c4f8e6f3d7b709f4f
atauzki <ps031a@gmail.com>
2024-04-30 11:55:13 +0800

    update terminal opertaion settings.

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -34,7 +34,6 @@ Ice.lsp.servers = {
     "tsserver",
 }
 
-
 -- Custom plugin: suda.vim for root/admin read and write.
 Ice.plugins.suda = {
     "lambdalisue/suda.vim",
@@ -96,3 +95,31 @@ Ice.plugins["nvim-treesitter"].opts = {
     },
 }
 
+-- Enhanced terminal operation settings.
+Ice.keymap.prefix["<leader>s"] = { name = "terminal" }
+if require("core.utils").is_windows() then
+    Ice.keymap.general.open_cmd = { "n", "<leader>sc", "<Cmd>split term://cmd<CR>" }
+    Ice.keymap.general.open_powershell = { "n", "<leader>sp", "<Cmd>split term://pwsh<CR>" }
+    -- TODO a way to launch vs devshell?
+else
+    Ice.keymap.general.open_bash = { "n", "<leader>ss", "<Cmd>split term://bash<CR>" }
+    Ice.keymap.general.open_zsh = { "n", "<leader>sz", "<Cmd>split term://zsh<CR>" }
+    -- Ice.keymap.general.open_fish = { "n", "<leader>sf", "<Cmd>split term://fish<CR>" }
+end
+Ice.keymap.general.open_lazygit = { "n", "<leader>gl", "<Cmd>split term://lazygit<CR>" }
+
+-- auto insert mode in terminal buffer.
+local term_operations = vim.api.nvim_create_augroup("TermOperations", { clear = true })
+vim.api.nvim_create_autocmd(
+    { "TermOpen", "WinEnter" },
+    { pattern = "term://*", group = term_operations, command = "startinsert" }
+)
+vim.api.nvim_create_autocmd({ "BufLeave" }, { pattern = "term://*", group = term_operations, command = "stopinsert" })
+
+-- auto close terminal buffer after program exits.
+vim.api.nvim_create_autocmd("TermClose", {
+    group = term_operations,
+    callback = function()
+        vim.cmd "close"
+    end,
+})
